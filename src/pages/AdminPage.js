25248eab4d3e4f73841599954d7987a888a77e8a
LeeJuhae <tlatmsdlwngo@naver.com>
2021-08-29 02:44:06 +0900

    CHKN-58 어드민 UI 템플릿을 로그 테이블에 반영
    서버에서 받아온 로그를 보여주는 로그 테이블의 UI를
    어드민 UI 템플릿을 사용하여 변경했습니다.

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,18 +1,62 @@
 import React, { useEffect, useRef, useState } from 'react';
-import SearchBar from '../components/SearchBar';
-import * as moment from 'moment';
-import '../styles/AdminPage.css';
-import { forceCheckOut, checkAdmin as getCheckAdmin } from '../api/api';
 import { useHistory } from 'react-router-dom';
+import * as moment from 'moment';
+
+import { makeStyles } from '@material-ui/core/styles';
+// core components
+import GridItem from '../admin/components/Grid/GridItem.js';
+import GridContainer from '../admin/components/Grid/GridContainer.js';
+import Table from '../admin/components/Table/Table.js';
+import Card from '../admin/components/Card/Card.js';
+import CardHeader from '../admin/components/Card/CardHeader.js';
+import CardBody from '../admin/components/Card/CardBody.js';
+import { Button } from '@material-ui/core';
+
+import SearchBar from '../components/SearchBar';
+import { forceCheckOut, checkAdmin as getCheckAdmin } from '../api/api';
+import '../styles/AdminPage.css';
+
+const styles = {
+  cardCategoryWhite: {
+    '&,& a,& a:hover,& a:focus': {
+      color: 'rgba(255,255,255,.62)',
+      margin: '0',
+      fontSize: '14px',
+      marginTop: '0',
+      marginBottom: '0'
+    },
+    '& a,& a:hover,& a:focus': {
+      color: '#FFFFFF'
+    }
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '500',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+    '& small': {
+      color: '#777',
+      fontSize: '65%',
+      fontWeight: '400',
+      lineHeight: '1'
+    }
+  }
+};
+
+const useStyles = makeStyles(styles);
 
 function AdminPage() {
   const history = useHistory();
-  const [LogType, setLogType] = useState(0);
-  const [Logs, setLogs] = useState([]);
-  const [Page, setPage] = useState(0);
+  const [logType, setLogType] = useState(0);
+  const [logs, setLogs] = useState([]);
+  const [page, setPage] = useState(0);
 
   const ref = useRef();
-
+  const classes = useStyles();
+  const tableHead = ['ID', '시간', '출/입', '인트라 ID', '카드 번호', '클러스터', '강제 퇴실'];
   const checkAdmin = async () => {
     try {
       const response = await getCheckAdmin();
@@ -104,49 +148,51 @@ function AdminPage() {
         <div
           style={{
             display: 'flex',
-            // border: "1px solid whitesmoke",
             width: '50%',
             padding: '1rem',
             height: '5rem'
           }}
         >
-          <SearchBar type={LogType} setLogs={setLogs} ref={ref} Page={Page} setPage={setPage} />
+          <SearchBar type={logType} setLogs={setLogs} ref={ref} Page={page} setPage={setPage} />
         </div>
       </div>
       <div style={{ overflowX: 'scroll', margin: 'auto' }}>
-        <div className='logWrapper'>
-          {LogType === 4 ? <div className='logBox1'>카드 번호</div> : null}
-          <div className='logBox3'>시간</div>
-          <div className='logBox1'>출/입</div>
-          <div className='logBox1'>로그인</div>
-          <div className='logBox1'>카드 번호</div>
-          <div className='logBox1'>클러스터</div>
-          <div className='logBox1'>강제 퇴실</div>
-        </div>
-        <hr />
-        {Logs &&
-          Logs.map((log, index) => {
-            const date = new Date(log.createdAt);
-            return (
-              <div key={index} className='logWrapper'>
-                {log.id ? <div className='logBox1'>{log.id}</div> : null}
-                {date ? (
-                  <div className='logBox3'>{moment(date).format('MM월 DD일 HH:mm')}</div>
-                ) : null}
-                {log.logType ? <div className='logBox1'>{log.logType}</div> : null}
-                {log.user ? <div className='logBox1'>{log.user.userName}</div> : null}
-                {log.card ? <div className='logBox1'>{log.card.cardId}</div> : null}
-                {log.card ? (
-                  <div className='logBox1'>{log.card.type === 0 ? '개포' : '서초'}</div>
-                ) : null}
-                {log.user ? (
-                  <div className='logBox1' data={log.user._id} onClick={checkOutOnClick}>
-                    {log.card.cardId === log.user.cardId ? '퇴실처리하기' : null}
-                  </div>
-                ) : null}
-              </div>
-            );
-          })}
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader color='primary'>
+                <h4 className={classes.cardTitleWhite}>Logs</h4>
+                <p className={classes.cardCategoryWhite}></p>
+              </CardHeader>
+              {logs && (
+                <CardBody>
+                  <Table
+                    tableHeaderColor='primary'
+                    tableHead={tableHead}
+                    tableData={logs.map((log, idx) => {
+                      const date = new Date(log.createdAt);
+                      return [
+                        log.id ?? page * 50 + idx + 1,
+                        moment(date).format('MM월 DD일 HH:mm') ?? null,
+                        log.logType ?? null,
+                        log.user ? log.user.userName : null,
+                        log.card ? log.card.cardId.toString() : null,
+                        log.card ? (log.card.type === 0 ? '개포' : '서초') : null,
+                        log.card ? (
+                          log.card.cardId === log.user.cardId ? (
+                            <Button variant='outlined' onClick={checkOutOnClick}>
+                              퇴실 처리
+                            </Button>
+                          ) : null
+                        ) : null
+                      ];
+                    })}
+                  />
+                </CardBody>
+              )}
+            </Card>
+          </GridItem>
+        </GridContainer>
       </div>
     </div>
   );
