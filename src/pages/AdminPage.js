983102bdd0c7c4228ae7ab98b22cf928c7e966f6
LeeJuhae <tlatmsdlwngo@naver.com>
2021-08-29 03:40:25 +0900

    CHKN-58 로그 필터별 버튼 클릭 콜백 함수 하나로 통합
    총 5개의 로그 필터 버튼(클러스터, 인트라 ID, 카드 번호, 미반납 카드, 모든 카드 정보) 클릭 시 실행되는
    콜백 함수를 하나로 통합했습니다.

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -17,18 +17,6 @@ import { forceCheckOut, checkAdmin as getCheckAdmin } from '../api/api';
 import '../styles/AdminPage.css';
 
 const styles = {
-  cardCategoryWhite: {
-    '&,& a,& a:hover,& a:focus': {
-      color: 'rgba(255,255,255,.62)',
-      margin: '0',
-      fontSize: '14px',
-      marginTop: '0',
-      marginBottom: '0'
-    },
-    '& a,& a:hover,& a:focus': {
-      color: '#FFFFFF'
-    }
-  },
   cardTitleWhite: {
     color: '#FFFFFF',
     marginTop: '0px',
@@ -71,33 +59,9 @@ function AdminPage() {
     checkAdmin();
   }, []);
 
-  const handleClusterButton = () => {
+  const handleFilterBtn = ({ target }) => {
     setLogs([]);
-    setLogType(0);
-    setPage(0);
-  };
-
-  const handleStudentButton = () => {
-    setLogs([]);
-    setLogType(1);
-    setPage(0);
-  };
-
-  const handleCardButton = () => {
-    setLogs([]);
-    setLogType(2);
-    setPage(0);
-  };
-
-  const handleCheckInButton = () => {
-    setLogs([]);
-    setLogType(3);
-    setPage(0);
-  };
-
-  const handleAllCaradButton = () => {
-    setLogs([]);
-    setLogType(4);
+    setLogType(+target.dataset.idx);
     setPage(0);
   };
 
@@ -129,20 +93,20 @@ function AdminPage() {
             justifyContent: 'center'
           }}
         >
-          <button className='filterBtn' onClick={handleClusterButton}>
-            클러스터 로그
+          <button className='filterBtn' data-idx='0' onClick={handleFilterBtn}>
+            클러스터
           </button>
-          <button className='filterBtn' onClick={handleStudentButton}>
-            학생 로그
+          <button className='filterBtn' data-idx='1' onClick={handleFilterBtn}>
+            인트라 ID
           </button>
-          <button className='filterBtn' onClick={handleCardButton}>
-            카드 로그
+          <button className='filterBtn' data-idx='2' onClick={handleFilterBtn}>
+            카드 번호
           </button>
-          <button className='filterBtn' onClick={handleCheckInButton}>
-            미반납 카뎃
+          <button className='filterBtn' data-idx='3' onClick={handleFilterBtn}>
+            미반납 카드
           </button>
-          <button className='filterBtn' onClick={handleAllCaradButton}>
-            모든 카드 정보 보기
+          <button className='filterBtn' data-idx='4' onClick={handleFilterBtn}>
+            모든 카드 정보
           </button>
         </div>
         <div
@@ -156,44 +120,42 @@ function AdminPage() {
           <SearchBar type={logType} setLogs={setLogs} ref={ref} Page={page} setPage={setPage} />
         </div>
       </div>
-      <div style={{ overflowX: 'scroll', margin: 'auto' }}>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12}>
-            <Card>
-              <CardHeader color='primary'>
-                <h4 className={classes.cardTitleWhite}>Logs</h4>
-                <p className={classes.cardCategoryWhite}></p>
-              </CardHeader>
-              {logs && (
-                <CardBody>
-                  <Table
-                    tableHeaderColor='primary'
-                    tableHead={tableHead}
-                    tableData={logs.map((log, idx) => {
-                      const date = new Date(log.createdAt);
-                      return [
-                        log.id ?? page * 50 + idx + 1,
-                        moment(date).format('MM월 DD일 HH:mm') ?? null,
-                        log.logType ?? null,
-                        log.user ? log.user.userName : null,
-                        log.card ? log.card.cardId.toString() : null,
-                        log.card ? (log.card.type === 0 ? '개포' : '서초') : null,
-                        log.card ? (
-                          log.card.cardId === log.user.cardId ? (
-                            <Button variant='outlined' onClick={checkOutOnClick}>
-                              퇴실 처리
-                            </Button>
-                          ) : null
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <Card>
+            <CardHeader color='primary'>
+              <h4 className={classes.cardTitleWhite}>Logs</h4>
+              <p className={classes.cardCategoryWhite}></p>
+            </CardHeader>
+            {logs && (
+              <CardBody>
+                <Table
+                  tableHeaderColor='primary'
+                  tableHead={tableHead}
+                  tableData={logs.map((log, idx) => {
+                    const date = new Date(log.createdAt);
+                    return [
+                      log.id ?? page * 50 + idx + 1,
+                      moment(date).format('MM월 DD일 HH:mm') ?? null,
+                      log.logType ?? null,
+                      log.user ? log.user.userName : null,
+                      log.card ? log.card.cardId.toString() : null,
+                      log.card ? (log.card.type === 0 ? '개포' : '서초') : null,
+                      log.card ? (
+                        log.card.cardId === log.user.cardId ? (
+                          <Button variant='outlined' onClick={checkOutOnClick}>
+                            퇴실 처리
+                          </Button>
                         ) : null
-                      ];
-                    })}
-                  />
-                </CardBody>
-              )}
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
+                      ) : null
+                    ];
+                  })}
+                />
+              </CardBody>
+            )}
+          </Card>
+        </GridItem>
+      </GridContainer>
     </div>
   );
 }
