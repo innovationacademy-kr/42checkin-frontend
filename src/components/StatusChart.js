99208ec9057b63ccb152d08f17ca5448cfdb57b8
이주해 <leejuhae821@gmail.com>
2021-10-08 13:43:16 +0900

    CHKN-103 로그인 페이지의 기존 컴포넌트 배치 변경
    css를 변경했습니다.

---
+++ src/components/StatusChart.js
@@ -0,0 +1,76 @@
+import { Doughnut } from 'react-chartjs-2';
+import { useSelector, shallowEqual } from 'react-redux';
+
+const StatusChart = () => {
+  const { maxGaepo, maxSeocho } = useSelector(
+    state => ({
+      maxGaepo: state.config.gaepo,
+      maxSeocho: state.config.seocho
+    }),
+    shallowEqual
+  );
+
+  const { gaepo, seocho } = useSelector(state => ({
+    gaepo: state.status.gaepo,
+    seocho: state.status.seocho
+  }));
+  console.log(gaepo, maxGaepo);
+
+  const gaepoData = {
+    datasets: [
+      {
+        data: [gaepo, maxGaepo - gaepo],
+        borderWidth: 1,
+        hoverBorderWidth: 3,
+        backgroundColor: ['rgba(83, 227, 173, 1)', 'rgba(83, 227, 173, 0.3)'],
+        fill: true
+      }
+    ]
+  };
+
+  const seochoData = {
+    datasets: [
+      {
+        data: [seocho, maxSeocho - seocho],
+        borderWidth: 1,
+        hoverBorderWidth: 3,
+        backgroundColor: ['rgba(109, 219, 226, 1)', 'rgba(109, 219, 226, 0.3)'],
+        fill: true
+      }
+    ]
+  };
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div>
+        <Doughnut
+          options={{
+            legend: {
+              display: true,
+              position: 'right'
+            },
+            animation: false,
+            cutout: '70%',
+            centertext: '123'
+          }}
+          data={gaepoData}
+          height={'30%'}
+        />
+      </div>
+      <div>
+        <Doughnut
+          options={{
+            legend: {
+              display: true,
+              position: 'right'
+            },
+            animation: false,
+            cutout: '70%'
+          }}
+          data={seochoData}
+          height={'30%'}
+        />
+      </div>
+    </div>
+  );
+};
+export default StatusChart;
