5a2ba14dddc533315bc46d755bd5f0f1c70f3973
강수호(sooohka) <shkorea1004@gmail.com>
2021-10-23 20:01:42 +0900

    CHKN-146 Config리듀서 이름 변경
    - 기존 config리듀서 cluster리듀서로 이름변경

--- src/utils/hooks/useCluster.ts
+++ src/utils/hooks/useCluster.ts
@@ -1,7 +1,11 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/modules";
+<<<<<<< HEAD
 import { setCluster as sC, setCurrentUserCount as sCUC } from "../../redux/modules/cluster";
+=======
+import { setConfig as sC, setCurrentUserCount as sCUC } from "../../redux/modules/cluster";
+>>>>>>> d7450ad (CHKN-146 Config리듀서 이름 변경)
 
 const useCluster = () => {
   const dispatch = useDispatch();
